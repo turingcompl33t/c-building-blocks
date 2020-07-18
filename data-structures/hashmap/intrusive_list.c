9ec2aa1558489e5a0fb71b4ce35145e4787d5254
turingcompl33t <turingcompl33t@gmail.com>
2020-07-18 19:30:47 -0400

    implementation for intrusive list

--- data-structures/hashmap/intrusive_list.c
+++ data-structures/hashmap/intrusive_list.c
@@ -2,32 +2,94 @@
 // Generic intrusive doubly-linked list data structure.
 
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "intrusive_list.h"
 
+// ----------------------------------------------------------------------------
+// Internal Declarations
+
+static bool list_is_empty(list_entry_t* head);
+
+// ----------------------------------------------------------------------------
+// Exported
+
 void list_init(list_entry_t* head)
 {
-    return;
+    // One might image using NULL values for flink and blink
+    // to denote an empty list, but this implementation makes
+    // the pointer swap operations cleaner in other functions.
+
+    head->flink = head;
+    head->blink = head;
 }
 
 void list_push_front(list_entry_t* head, list_entry_t* entry)
 {
-    return;
+    if (NULL == head || NULL == entry)
+    {
+        return;
+    }
+
+    if (list_is_empty(head))
+    {
+        head->blink = entry;
+    }
+
+    entry->flink = head->flink;
+    entry->blink = head;
+
+    head->flink->blink = entry;
+    head->flink = entry;
 }
 
 void list_push_back(list_entry_t* head, list_entry_t* entry)
 {
-    return;
+    if (NULL == head || NULL == entry)
+    {
+        return;
+    }
+
+    if (list_is_empty(head))
+    {
+        head->flink = entry;
+    }
+
+    entry->blink = head->blink;
+    entry->flink = head;
+
+    head->blink->flink = entry;
+    head->blink = entry;
 }
 
 list_entry_t* list_pop_front(list_entry_t* head)
 {
-    return NULL;
+    if (NULL == head || list_is_empty(head))
+    {
+        return NULL;
+    }
+
+    list_entry_t* popped = head->flink;
+
+    popped->flink->blink = popped->blink;
+    popped->blink->flink = popped->flink;
+
+    return popped;
 }
 
 list_entry_t* list_pop_back(list_entry_t* head)
 {
-    return NULL;
+    if (NULL == head || list_is_empty(head))
+    {
+        return NULL;
+    }
+
+    list_entry_t* popped = head->blink;
+
+    popped->flink->blink = popped->blink;
+    popped->blink->flink = popped->flink;
+
+    return popped;
 }
 
 void list_remove_entry(list_entry_t* head, list_entry_t* entry)
@@ -37,5 +99,25 @@ void list_remove_entry(list_entry_t* head, list_entry_t* entry)
 
 size_t list_count(list_entry_t* head)
 {
-    return 0;
+    if (NULL == head || list_is_empty(head))
+    {
+        return 0;
+    }
+
+    size_t count = 0;
+    list_entry_t* current;
+    for (current = head->flink; current != head; current = current->flink)
+    {
+        ++count;
+    }
+
+    return count;
+}
+
+// ----------------------------------------------------------------------------
+// Internal
+
+static bool list_is_empty(list_entry_t* head)
+{
+    return (head->flink == head) && (head->blink == head);
 }
