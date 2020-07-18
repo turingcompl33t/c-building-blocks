2b18c5f4579705f370a900fce9b87cd9a4ca96c9
turingcompl33t <turingcompl33t@gmail.com>
2020-07-18 11:30:13 -0400

    initial implementation for reference stack

--- data-structures/stack/stack.c
+++ data-structures/stack/stack.c
@@ -1,7 +1,12 @@
 // stack.c
 // Generic stack (last in, first out) data structure.
 
+#include <stdlib.h>
+
 #include "stack.h"
+
+// ----------------------------------------------------------------------------
+// Internal Types
 
 // An individual item on the stack.
 typedef struct stack_item
@@ -13,32 +18,108 @@ typedef struct stack_item
     void* data;
 } stack_item_t;
 
+// ----------------------------------------------------------------------------
+// Internal Prototypes
+
+static stack_item_t* new_item(void* data);
+
+// ----------------------------------------------------------------------------
+// Exported
+
 stack_t* stack_new(void)
 {
-    return NULL;
+    stack_t* stack = malloc(sizeof(stack_t));
+    if (NULL == stack)
+    {
+        return NULL;
+    }
+
+    stack->top   = NULL;
+    stack->count = 0;
+
+    return stack;
 }
 
 bool stack_delete(stack_t* stack)
 {
-    return false;
+    if (NULL == stack || stack->count > 0)
+    {
+        return false;
+    }
+
+    // the stack is empty, safe to delete
+    free(stack);
+    stack = NULL;
+
+    return true;
 }
 
 bool stack_push(stack_t* stack, void* data)
 {
-    return false;
+    if (NULL == stack)
+    {
+        return false;
+    }
+
+    stack_item_t* item = new_item(data);
+    if (NULL == item)
+    {
+        return false;
+    }
+
+    item->below = stack->top;
+
+    stack->top = item;
+    stack->count++;
+
+    return true;
 }
 
 void* stack_pop(stack_t* stack)
 {
-    return NULL;
+    if (NULL == stack || stack->count == 0)
+    {
+        return NULL;
+    }
+
+    stack_item_t* popped = stack->top;
+
+    stack->top = popped->below;
+    stack->count--;
+
+    void* data = popped->data;
+
+    free(popped);
+
+    return data;
 }
 
 void* stack_peek(stack_t* stack)
 {
-    return NULL;
+    if (NULL == stack || stack->count == 0)
+    {
+        return NULL;
+    }
+
+    return stack->top->data;
 }
 
 size_t stack_count(stack_t* stack)
 {
-    return 0;
+    return (NULL == stack) ? 0 : stack->count; 
+}
+
+// ----------------------------------------------------------------------------
+// Internal
+
+static stack_item_t* new_item(void* data)
+{
+    stack_item_t* item = malloc(sizeof(stack_item_t));
+    if (NULL == item)
+    {
+        return NULL;
+    }
+
+    item->data = data;
+    return item;
 }
