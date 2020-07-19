e123caccd0ed1cd93abe13ffc26e45a1a12a38c8
turingcompl33t <turingcompl33t@gmail.com>
2020-07-18 22:28:26 -0400

    hashmap wip again

--- data-structures/hashmap/hashmap.c
+++ data-structures/hashmap/hashmap.c
@@ -14,7 +14,25 @@
 // Bins in our hashtable will simply be intrusive list headers.
 typedef list_entry_t bucket_t;
 
+typedef struct bucket_item
+{
+    // The embedded intrusive list entry
+    list_entry_t entry;
+
+    // Key and value pointers
+    void* key;
+    void* value;
+
+    // Memoize the hash, not strictly necessary, but saves during resize.
+    hash_t hash;
+} bucket_item_t;
+
 static void initialize_table(bucket_t* buckets, size_t n);
+
+static void flush_bucket(bucket_t* bucket_head, deleter_f deleter);
+
+static bucket_item_t* new_bucket_item(void* key, void* value, hash_t hash);
+static void delete_bucket_item(bucket_item_t* item);
 
 // ----------------------------------------------------------------------------
 // Exported
@@ -50,7 +68,8 @@ hashmap_t* hashmap_new(
     map->comparator = comparator;
     map->deleter    = deleter;
 
-    map->count = 0;
+    map->count     = 0;
+    map->n_buckets = INIT_N_BUCKETS;
 
     return map;
 }
@@ -61,6 +80,13 @@ void hashmap_delete(hashmap_t* map)
     {
         return;
     }
+
+    for (size_t i = 0; i < map->n_buckets; ++i)
+    {
+        flush_bucket(&(map->buckets[i]), map->deleter);
+    }
+
+    free(map);
 }
 
 bool hashmap_insert(
@@ -69,7 +95,14 @@ bool hashmap_insert(
     void* value, 
     void** out)
 {
-    return false;
+    if (NULL == map)
+    {
+        return false;
+    } 
+
+    *out = NULL;
+
+    return true;
 }
 
 bool hashmap_remove(hashmap_t* map, void* key)
@@ -84,7 +117,7 @@ void* hashmap_find(hashmap_t* map, void* key)
 
 size_t hashmap_count(hashmap_t* map)
 {
-    return 0;
+    return (NULL == map) ? 0 : map->count;
 }
 
 // ----------------------------------------------------------------------------
@@ -96,4 +129,34 @@ static void initialize_table(bucket_t* buckets, size_t n)
     {
         list_init(&buckets[i]);
     }
+}
+
+static void flush_bucket(bucket_t* bucket_head, deleter_f deleter)
+{
+    bucket_item_t* item;
+    while ((item = (bucket_item_t*) list_pop_front(bucket_head)) != NULL)
+    {
+        deleter(item->value);
+        delete_bucket_item(item);
+    }
+}
+
+static bucket_item_t* new_bucket_item(void* key, void* value, hash_t hash)
+{
+    bucket_item_t* item = malloc(sizeof(bucket_item_t));
+    if (NULL == item)
+    {
+        return NULL;
+    }
+
+    item->key   = key;
+    item->value = value;
+    item->hash  = hash;
+
+    return item;
+}
+
+static void delete_bucket_item(bucket_item_t* item)
+{
+    free(item);
 }
