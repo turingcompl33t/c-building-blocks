56da780f542b9a8470e5050a6559c7480927ad6e
turingcompl33t <turingcompl33t@gmail.com>
2020-07-22 21:17:56 -0400

    reference impl for mergesort

--- algorithms/mergesort/mergesort.c
+++ algorithms/mergesort/mergesort.c
@@ -1,8 +1,81 @@
 // mergesort.c
 
+#include <string.h>
+
 #include "mergesort.h"
+
+// ----------------------------------------------------------------------------
+// Internal Declarations
+
+static void split_and_merge(
+    int dst[], 
+    int src[], 
+    size_t lo, 
+    size_t hi, 
+    comparator_f cmp);
+
+static void merge(
+    int dst[], 
+    int src[], 
+    size_t lo, 
+    size_t mid, 
+    size_t hi, 
+    comparator_f cmp);
+
+// ----------------------------------------------------------------------------
+// Exported
 
 void mergesort(int src[], int dst[], size_t lo, size_t hi, comparator_f cmp)
 {
-    return;
+    memcpy(dst, src, sizeof(int)*(hi - lo));
+    split_and_merge(dst, src, lo, hi + 1, cmp);
+}
+
+// ----------------------------------------------------------------------------
+// Internal
+
+static void split_and_merge(
+    int dst[], 
+    int src[], 
+    size_t lo, 
+    size_t hi, 
+    comparator_f cmp)
+{
+    if (hi - lo <= 1)
+    {
+        return;
+    }
+
+    const size_t mid = (hi + lo) / 2;
+    
+    split_and_merge(src, dst, lo, mid, cmp);
+    split_and_merge(src, dst, mid, hi, cmp);
+
+    merge(dst, src, lo, mid, hi, cmp);
+}
+
+static void merge(
+    int dst[], 
+    int src[], 
+    size_t lo, 
+    size_t mid, 
+    size_t hi, 
+    comparator_f cmp)
+{
+    size_t i = lo;
+    size_t j = mid;
+
+    for (size_t k = lo; k < hi; ++k)
+    {
+        if (i < mid && (j >= hi || cmp(src[i], src[j])))
+        {
+            dst[k] = src[i];
+            ++i;
+        }
+        else
+        {
+            dst[k] = src[j];
+            ++j;
+        }
+    }
 }
